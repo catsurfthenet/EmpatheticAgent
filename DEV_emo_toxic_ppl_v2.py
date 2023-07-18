1f6d935082a3ba5b00ef6bd53758aa4f707cbed7
cannice <sadkitty08@gmail.com>
2023-07-18 22:10:56 +0100

    debugged repetition problem

--- DEV_emo_toxic_ppl_v2.py
+++ DEV_emo_toxic_ppl_v2.py
@@ -42,7 +42,7 @@ from nltk.tokenize import word_tokenize
 #from classifiers import get_sentence_score
 from scipy.spatial import distance
 from scipy.special import softmax
-from helper import get_mean, weighted_bleu_score, get_js_distance, emo_count_ppl_toxic, emo_dis_ppl, emo_dis_ppl_toxic, load_toxicity_classifier, load_empathy_classifier, load_emo_classifier, emo_dis_bleu, append_scores
+from helper import build_dataset, build_train_dataset, build_pad_dataset, get_mean, weighted_bleu_score, get_js_distance, emo_count_ppl_toxic, emo_dis_ppl, emo_dis_ppl_toxic, load_toxicity_classifier, load_empathy_classifier, load_emo_classifier, emo_dis_bleu, append_scores
 from torch.utils.data import DataLoader
 tqdm.pandas()
 
@@ -64,34 +64,39 @@ tqdm.pandas()
 ########################################################################
 """ED: empathetic dialogues, RT: real toxic"""
 # define path and variables
-input_batch_size = 24
-input_mini_batch_size = 4
-optimiser_choice = "Adam"
-reward_function = emo_dis_ppl_toxic
-train_dataset_path = "modeldata/input_resp_clean_prompt_emo_train_dialogue_dataset.p" # 'modeldata/emo_dis_train_dialogue_dataset.p' #
-save_path_prefix = f"test5_clean_emo_respxppl_ED_{optimiser_choice}_bs{input_batch_size}_lr-5_gamma0.99_emo_probi_w5-0-5" #"DEV_lr-7_ppl_toxic_w4-6-0" #"DEV-mimic-lr-6-ppl-toxic" # "DEV_SGD_lr-9_emo_toxic_w6-4-0"
-load_path_prefix = "./"
-ppo_model = f"{load_path_prefix}DEV_lr-9_ppl_toxic_w6-4-0-blenderbot-400m-emo-probi-ppl-last-score0.6292313380390405-ppl4.034670352935791"
-blenderbot = "facebook/blenderbot-400M-distill"
-dialogpt = "microsoft/DialoGPT-medium"
-opt_iml = "facebook/opt-iml-1.3b"
-model_path = blenderbot
+input_batch_size = 24 # [8, 12, 16, 20, 24, 28]
+optimiser_choice = "Adam" # ["Adam", "AdamW", "SGD"]
+reward_function = emo_dis_ppl_toxic #[emo_dis_ppl_toxic, emo_count_ppl_toxic]
 # define weights
-emp_weight = 0.5 #0
-toxicity_weight = 0
-fluency_weight = 0.5 #1
-ref_gen_emo_match_reward = 2
-lr = 1.47e-5
+emp_weight = 1 #0 # np.arange(0, 1, 0.1)
+toxicity_weight = 0 # np.arange(0, 1, 0.1),  + emp_weight <= 1
+fluency_weight = 0 #1 # 1 - emp_weight - toxicity_weight
+ref_gen_emo_match_reward = 5 # np.arange(0, 9, 1)
+reward_scale = 10 # np.arange(1, 10, 1)
+
+lr = 1.47e-2
 weight_decay = 0.001
-ppo_epoch_num = 5
-score_min = 100
-score_max = 0
+ppo_epoch_num = 10
 DEV = True
 train_set_size = 3000 #3000
 dev_set_size = 80
 checkpoint = 50
-epoch_num = 1
-reward_scale = 3
+epoch_num = 1 # number of outer loops
+
+train_dataset_path = "modeldata/input_resp_clean_prompt_emo_train_dialogue_dataset.p" # 'modeldata/emo_dis_train_dialogue_dataset.p' #
+save_path_prefix = f"test7_ep10_clean_emo_resp5xppl_ED_{optimiser_choice}_bs{input_batch_size}_lr-5_gamma0.99_emo_probi_w1-0-0" #"DEV_lr-7_ppl_toxic_w4-6-0" #"DEV-mimic-lr-6-ppl-toxic" # "DEV_SGD_lr-9_emo_toxic_w6-4-0"
+load_path_prefix = "./"
+ppo_model = f"{load_path_prefix}DEV_lr-9_ppl_toxic_w6-4-0-blenderbot-400m-emo-probi-ppl-last-score0.6292313380390405-ppl4.034670352935791"
+
+
+# fixed variables
+score_min = 100
+score_max = 0
+input_mini_batch_size = 4
+blenderbot = "facebook/blenderbot-400M-distill"
+dialogpt = "microsoft/DialoGPT-medium"
+opt_iml = "facebook/opt-iml-1.3b"
+model_path = blenderbot
 
 device = torch.cuda.current_device() if torch.cuda.is_available() else "cpu"
 # We first define the configuration of the experiment, defining the model, the dataset,
@@ -104,9 +109,10 @@ class ScriptArguments:
     """
     The name of the Casual LM model we wish to fine with PPO
     """
+
     # NOTE: gpt2 models use Conv1D instead of Linear layers which are not yet supported in 8 bit mode
     # models like gpt-neo* models are more suitable.
-    model_name: Optional[str] = field(default="facebook/blenderbot-400M-distill", metadata={"help": "the model name"})
+    model_name: Optional[str] = field(default=model_path, metadata={"help": "the model name"})
     log_with: Optional[str] = field(default=None, metadata={"help": "use 'wandb' to log with wandb"})
     learning_rate: Optional[float] = field(default=(lr) * 2, metadata={"help": "the learning rate"})
     mini_batch_size: Optional[int] = field(default=input_mini_batch_size, metadata={"help": "the PPO minibatch size"})
@@ -152,105 +158,12 @@ config = PPOConfig(
 # Below is an example function to build the dataset. In our case, we use the IMDB dataset
 # from the `datasets` library. One should customize this function to train the model on
 # its own dataset.
-def build_dataset(
-    config, dataset_path='modeldata/dialogue_dataset.p', input_min_text_length=5, input_max_text_length=100, size=-1
-):
-    """
-    Build dataset for training. This builds the dataset from `load_dataset`, one should
-    customize this function to train the model on its own dataset.
-
-    Args:
-        dataset_name (`str`):
-            The name of the dataset to be loaded.
-
-    Returns:
-        dataloader (`torch.utils.data.DataLoader`):
-            The dataloader for the dataset.
-    """
-    tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-    tokenizer.pad_token = tokenizer.eos_token
-
-    #ds = load_dataset(dataset_name, split="train")
-    if (os.path.exists(dataset_path)):
-        print("LOADING empathetic_dialogue")
-        with open(dataset_path, "rb") as f:
-            [data] = pickle.load(f)
-    ds = Dataset.from_dict(data)
-
-    input_size = LengthSampler(input_min_text_length, input_max_text_length)
-
-    def tokenize(sample):
-        prompt = sample["prompt"] # prompt
-        continuation = sample["target"] # utterance
-
-        sample["input_ids"] = tokenizer.encode(prompt)[: input_size()]
-        sample["input_ids"] += [0] * max((128 - len(sample["input_ids"])), 0)
-        #sample["target_ids"] = tokenizer.encode(continuation)[: input_size()]
-        sample["query"] = {"prompt": tokenizer.decode(sample["input_ids"]),
-                           "target": continuation}
-        return sample
-
-    ds = ds.map(tokenize, batched=False)
-    ds.set_format(type="torch")
-
-    ds = ds.train_test_split(test_size=0.2, shuffle=False)["train"]
-    if size > -1:
-        ds = ds.shuffle(seed=2023).select(range(size))
-    return ds
-
-def build_train_dataset(
-    config, dataset_path='modeldata/dialogue_dataset.p', input_min_text_length=5, input_max_text_length=100, size=-1
-):
-    """
-    Build dataset for training. This builds the dataset from `load_dataset`, one should
-    customize this function to train the model on its own dataset.
-
-    Args:
-        dataset_name (`str`):
-            The name of the dataset to be loaded.
-
-    Returns:
-        dataloader (`torch.utils.data.DataLoader`):
-            The dataloader for the dataset.
-    """
-    tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-    tokenizer.pad_token = tokenizer.eos_token
-
-    #ds = load_dataset(dataset_name, split="train")
-    if (os.path.exists(dataset_path)):
-        print("LOADING empathetic_dialogue")
-        with open(dataset_path, "rb") as f:
-            [data] = pickle.load(f)
-    ds = Dataset.from_dict(data)
-
-    input_size = LengthSampler(input_min_text_length, input_max_text_length)
-
-    def tokenize(sample):
-        prompt = sample["prompt"] # prompt
-        continuation = sample["target"] # utterance
-        target_emotion = sample["target_emo"]
-
-        sample["input_ids"] = tokenizer.encode(prompt)[: input_size()]
-        sample["input_ids"] += [0] * max((128 - len(sample["input_ids"])), 0)
-        #sample["target_ids"] = tokenizer.encode(continuation)[: input_size()]
-        sample["query"] = {"prompt": tokenizer.decode(sample["input_ids"]),
-                           "target": continuation,
-                           "target_emo": target_emotion}
-        return sample
-
-    ds = ds.map(tokenize, batched=False)
-    ds.set_format(type="torch")
-
-    ds = ds.train_test_split(test_size=0.2, shuffle=False)["train"]
-    if size > -1:
-        ds = ds.shuffle(seed=2023).select(range(size))
-    return ds
 
 # We retrieve the dataloader by calling the `build_dataset` function.
 min_input_length = 30
 max_input_length = 100
 dataset = build_train_dataset(config, dataset_path=train_dataset_path, input_min_text_length=min_input_length, input_max_text_length=max_input_length, size=train_set_size)
-dev_dataset = build_dataset(config, dataset_path='modeldata/dev_dialogue_dataset.p', input_min_text_length=min_input_length, input_max_text_length=max_input_length, size=dev_set_size)
+dev_dataset = build_pad_dataset(config, dataset_path='modeldata/dev_dialogue_dataset.p', input_min_text_length=min_input_length, input_max_text_length=max_input_length, size=dev_set_size)
 dev_dataloader = DataLoader(dev_dataset, batch_size=8, shuffle=False)
 
 def collator(data):
@@ -279,7 +192,6 @@ elif optimiser_choice == "SGD":
     optimizer = SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=config.learning_rate)
 else: # use Adam as default
     optimizer = Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=config.learning_rate)
-#
 
 # only for this model.
 tokenizer = AutoTokenizer.from_pretrained(config.model_name)
@@ -342,7 +254,6 @@ mean_emo_list = []
 mean_toxic_list = []
 list_stats = []
 short_length_penalty = 5
-#loss_list = []
 #epoch_num = 0
 #init = time.time()
 with open(f'{save_path_prefix}_score_train_output.txt', 'w') as f:
@@ -374,8 +285,7 @@ with open(f'{save_path_prefix}_score_train_output.txt', 'w') as f:
             texts = batch["response"]
             prompts = [q.get("prompt").replace("</s>", "").replace("_comma_", ",") for q in batch["query"]]
             response_tensors_t = torch.stack(response_tensors)
-            #test = ppo_trainer.model(input_ids=response_tensors_t, labels=response_tensors_t)
-            # TODO use ref_model, or other models
+            # use ref_model, or other models
             loss = blenderbot_model(input_ids=response_tensors_t, labels=response_tensors_t).loss # get loss
             #loss_list.append(loss)
             #loss = criterion()
